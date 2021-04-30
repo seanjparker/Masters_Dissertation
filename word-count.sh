53273d5319e4004479b7e0a0a83d511d52f84ba7
Sean Parker <sean.parker067@gmail.com>
2021-04-30 03:44:51 +0100

    Update wc script to print raw results with any arg

--- word-count.sh
+++ word-count.sh
@@ -2,4 +2,8 @@
 
 wordc=$(texcount -inc -sum "acs-dissertation.tex" | grep "Sum count:" | tail -n1 | cut -d " " -f 3)
 
-echo "Total Words: ${wordc}"
+if [[ -n $1 ]]; then
+  echo "$(texcount -inc -sum acs-dissertation.tex)"
+else
+  echo "Total Words: ${wordc}"
+fi
